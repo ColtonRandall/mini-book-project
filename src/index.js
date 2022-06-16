1403e4bcbb13a517a5174655610a9185a2600065
Colton Randall <colton.randall@xero.com>
2022-06-17 08:53:54 +1200

    update README

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import CSS
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Stateless functional component
+// Components must be capitalised
+
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
+  title: "Why a Daughter Needs a Dad",
+  author: "Gregory E. Lang",
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL906_SR906,600_.jpg",
+  title: "Atomic Habits",
+  author: "James Clear",
+};
+
+function BookList() {
+  return (
+    <section className="bookList">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
+    </section>
+  );
+}
+
+const Book = ({ img, author, title }) => {
+  // const { img, title, author } = props;
+
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+// Same thing, different syntax
+// this will get messay VERY fast
+
+// const Greeting = () => {
+//   return React.createElement("h3", {}, "Colton's second component");
+// };
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
