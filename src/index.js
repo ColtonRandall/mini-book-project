3b6ec0505f39a37d1f63b653aeb7407c6ff4c060
Colton Randall <colton.randall@xero.com>
2022-06-20 09:07:55 +1200

    upadted to iterate through list of objects

--- src/index.js
+++ src/index.js
@@ -4,66 +4,53 @@ import ReactDOM from "react-dom";
 // import CSS
 import "./index.css";
 
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
+    title: "Why a Daughter Needs a Dad",
+    author: "Gregory E. Lang",
+  },
+
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL906_SR906,600_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/61m1Vxw8tiL._AC_UL604_SR604,400_.jpg",
+    title: "Where the Crawdads Sing",
+    author: "Delia Owens",
+  },
+];
+
 // Stateless functional component
 // Components must be capitalised
-
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81F30JDZU9L._AC_UL604_SR604,400_.jpg",
-  title: "Why a Daughter Needs a Dad",
-  author: "Gregory E. Lang",
-};
-
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL906_SR906,600_.jpg",
-  title: "Atomic Habits",
-  author: "James Clear",
-};
-
 function BookList() {
   return (
-    <section className="bookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        {/* Children props go witihin two Component tags */}
-        <p>
-          A New York Times bestseller! The perfect Father's Day gift, birthday
-          present for dad, Valentine's Day book, or sweet keepsake for a
-          daughter of any age!A sweet picture book showcasing all the ways a
-          father will help his daughter grow, this is the gift for every girl
-          dad and little girl to celebrate their special bond,
-        </p>
-      </Book>
+    /* Iterating each book to render on page */
 
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+    <section className="bookList">
+      {books.map((book) => {
+        // 'book' prop is equal to book
+        // spread operator - to bring in all props coming from books.map(book)
+        // Remember to attach a unique id to each object
+        return <Book key={book.id} {...book} />;
+      })}
     </section>
   );
 }
 
-const Book = ({ img, author, title, children }) => {
-  // const { img, title, author } = props;
-
+const Book = ({ img, title, author }) => {
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
-
-// Same thing, different syntax
-// this will get messay VERY fast
-
-// const Greeting = () => {
-//   return React.createElement("h3", {}, "Colton's second component");
-// };
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
